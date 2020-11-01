e044d4d9a8bfccbc03f994fb0762423e84ccea33
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-01 18:24:06 -0500

    Initial Commit

---
+++ Hyped List/Hyped_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hyped_ListApp.swift
+//  Hyped List
+//
+//  Created by Ryan Spishock on 11/1/20.
+//
+
+import SwiftUI
+
+@main
+struct Hyped_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
