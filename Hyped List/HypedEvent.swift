ca03a0040d79f54b10c9d410f9472c4955c1951a
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-04 13:58:35 -0500

    Add ScrollView and Event logic

--- Hyped List/HypedEvent.swift
+++ Hyped List/HypedEvent.swift
@@ -8,7 +8,8 @@
 import Foundation
 import SwiftUI
 
-class HypedEvent: ObservableObject {
+class HypedEvent: ObservableObject, Identifiable {
+    var id = UUID().uuidString
     var date = Date()
     var title = ""
     var url = ""
@@ -23,4 +24,31 @@ class HypedEvent: ObservableObject {
         }
         return nil
     }
+}
+
+var testHypedEvent1: HypedEvent {
+    let hypedEvent = HypedEvent()
+    
+    if let image = UIImage(named: "wwdc") {
+        if let data = image.pngData() {
+            hypedEvent.imageData = data
+        }
+    }
+    
+    hypedEvent.title = "WWDC 2021"
+    hypedEvent.color = .green
+    hypedEvent.date = Date()
+    hypedEvent.url = "http://www.apple.com"
+    
+    return hypedEvent
+}
+
+var testHypedEvent2: HypedEvent {
+    let hypedEvent = HypedEvent()
+    
+    hypedEvent.title = "Trip to Silicon Valley"
+    hypedEvent.color = .gray
+    hypedEvent.date = Date()
+    
+    return hypedEvent
 }
--- Hyped List/UpcomingView.swift
+++ Hyped List/UpcomingView.swift
@@ -10,24 +10,52 @@ import SwiftUI
 struct UpcomingView: View {
     
     @State var showingCreateView = false
+    var hypedEvents: [HypedEvent] = []
     
     var body: some View {
-        Text("Upcoming")
-            .navigationTitle("Upcoming")
-            .navigationBarItems(trailing:
-                                    Button(action: {showingCreateView = true}) {
-                                        Image(systemName: "calendar.badge.plus")
-                                            .font(.title)
-                                    }
-                                    .sheet(isPresented: $showingCreateView) {
-                                        CreateHypedEventView()
-                                    }
-            )
-    }
-}
+        ScrollView {
+            VStack {
+                if hypedEvents.count == 0 {
+                    Text("Nothing to look forward to 😢\nCreate an event or check out the Discovery tab!")
+                        .bold()
+                        .multilineTextAlignment(.center)
+                } else {
+                    ForEach(hypedEvents) { hypedEvent in
+                        Text(hypedEvent.title)
+                        Circle().foregroundColor(hypedEvent.color)
+                    }   // ForEach
+                }       // if statement
+                
+            }           // Vstack
+        }           // ScrollView
+        .navigationTitle("Upcoming")
+        .navigationBarItems(trailing:
+                                Button(action: {showingCreateView = true}) {
+                                    Image(systemName: "calendar.badge.plus")
+                                        .font(.title)
+                                }   // Button
+                                .sheet(isPresented: $showingCreateView) {
+                                    CreateHypedEventView()
+                                }   // .sheet
+        )
+    }               // View
+}                   // body
 
 struct UpcomingView_Previews: PreviewProvider {
     static var previews: some View {
-        UpcomingView()
-    }
-}
+        
+        Group {
+            NavigationView {
+                // Populated view
+                UpcomingView(hypedEvents: [testHypedEvent1, testHypedEvent2])
+            }       //  NavigationView
+        }           // Group
+        
+        Group {
+            NavigationView {
+                // Empty view
+                UpcomingView(hypedEvents: [])
+            }       // NavigationView
+        }           // Group
+    }               // previews
+}                   // PreviewProvider
